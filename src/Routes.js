f9efb59adbaed46597caa3b4b89350a5209056eb
mdajminpradhan <md.ajminpradhan@outlook.com>
2021-10-19 00:30:13 +0600

    initial commit

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+// pages
+import Home from '../pages/site/Home'
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path={Home} excat component={Home} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes
